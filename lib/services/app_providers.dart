bc2312c7fb38dbfdf6d2b21f47750b19e0b1f005
Abdul Haseeb <62133331+askabdulhaseeb@users.noreply.github.com>
2024-10-16 18:23:28 +0500

    services path update

--- lib/services/app_providers.dart
+++ lib/services/app_providers.dart
@@ -1,9 +1,9 @@
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
-import 'features/personal/dashboard/views/providers/personal_bottom_nav_provider.dart';
+import '../features/personal/dashboard/views/providers/personal_bottom_nav_provider.dart';
 import 'get_it.dart';
 
-import 'features/personal/auth/signin/views/providers/signin_provider.dart';
+import '../features/personal/auth/signin/views/providers/signin_provider.dart';
 
 final List<SingleChildWidget> appProviders = <SingleChildWidget>[
   // Add your providers here
--- lib/services/get_it.dart
+++ lib/services/get_it.dart
@@ -1,11 +1,11 @@
 import 'package:get_it/get_it.dart';
 
-import 'features/personal/auth/signin/data/repositories/signin_repository_impl.dart';
-import 'features/personal/auth/signin/data/sources/signin_remote_source.dart';
-import 'features/personal/auth/signin/domain/repositories/signin_repository.dart';
-import 'features/personal/auth/signin/domain/usecase/forgot_password_usecase.dart';
-import 'features/personal/auth/signin/domain/usecase/login_usecase.dart';
-import 'features/personal/auth/signin/views/providers/signin_provider.dart';
+import '../features/personal/auth/signin/data/repositories/signin_repository_impl.dart';
+import '../features/personal/auth/signin/data/sources/signin_remote_source.dart';
+import '../features/personal/auth/signin/domain/repositories/signin_repository.dart';
+import '../features/personal/auth/signin/domain/usecase/forgot_password_usecase.dart';
+import '../features/personal/auth/signin/domain/usecase/login_usecase.dart';
+import '../features/personal/auth/signin/views/providers/signin_provider.dart';
 
 final GetIt locator = GetIt.instance;
 
