a243914fa4019671a52fb363dcfa33eb95856d84
Abdul Haseeb <62133331+askabdulhaseeb@users.noreply.github.com>
2024-11-15 16:48:59 +0500

    update Path

---
+++ lib/features/personal/cart/basket/domain/entities/cart_item_entity.dart
@@ -0,0 +1,35 @@
+import 'package:hive/hive.dart';
+
+import '../../../../../../core/enums/cart/cart_item_type.dart';
+part 'cart_item_entity.g.dart';
+
+@HiveType(typeId: 38)
+class CartItemEntity {
+  CartItemEntity({
+    required this.quantity,
+    required this.postID,
+    required this.listID,
+    required this.color,
+    required this.size,
+    required this.cartItemID,
+    required this.status,
+  });
+
+  @HiveField(0)
+  int quantity;
+  @HiveField(1)
+  final String postID;
+  @HiveField(2)
+  final String listID;
+  @HiveField(3)
+  final String? color;
+  @HiveField(4)
+  final String? size;
+  @HiveField(5)
+  final String cartItemID;
+  @HiveField(6)
+  String status;
+
+  bool get inCart => status == 'cart';
+  CartItemType get type => inCart ? CartItemType.cart : CartItemType.saveLater;
+}
---
+++ lib/features/personal/cart/basket/views/widgets/cart_save_later_toggle_section.dart
@@ -0,0 +1,28 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../../../../core/enums/cart/cart_item_type.dart';
+import '../../../../../../core/widgets/custom_toggle_switch.dart';
+import '../providers/cart_provider.dart';
+
+class CartSaveLaterToggleSection extends StatelessWidget {
+  const CartSaveLaterToggleSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<CartProvider>(builder: (
+      BuildContext context,
+      CartProvider cartPro,
+      _,
+    ) {
+      return CustomToggleSwitch<CartItemType>(
+        labels: CartItemType.list,
+        onToggle: (CartItemType value) => cartPro.basketPage = value,
+        initialValue: cartPro.basketPage,
+        labelStrs: CartItemType.codeList.map((String e) => e.tr()).toList(),
+        labelText: '',
+      );
+    });
+  }
+}
