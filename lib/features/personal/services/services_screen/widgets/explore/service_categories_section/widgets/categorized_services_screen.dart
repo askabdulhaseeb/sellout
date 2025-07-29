7d2018bc03ad10b0798f187b297aff9933ce224e
ahmershurahbeeljan <ahmershurahbeeljan@gmail.com>
2025-07-28 17:42:36 -0700

    service category title

--- lib/features/personal/services/services_screen/widgets/explore/service_categories_section/widgets/categorized_services_screen.dart
+++ lib/features/personal/services/services_screen/widgets/explore/service_categories_section/widgets/categorized_services_screen.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../../../../../../../core/widgets/scaffold/app_bar/app_bar_title_widget.dart';
 import '../../../../../../../business/core/domain/entity/service/service_entity.dart';
 import '../../../../providers/services_page_provider.dart';
 import '../../../../../service_detail/widget/service_card/service_card.dart';
@@ -11,9 +12,13 @@ class CategorizedServicesScreen extends StatelessWidget {
   static const String routeName = '/categorized-services';
   @override
   Widget build(BuildContext context) {
+    final ServicesPageProvider pro =
+        Provider.of<ServicesPageProvider>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
-        title: Text('services'.tr()),
+        title: AppBarTitle(
+          titleKey: pro.selectedCategory?.code.tr() ?? 'na'.tr(),
+        ),
       ),
       body: Consumer<ServicesPageProvider>(
         builder: (BuildContext context, ServicesPageProvider pro, _) {
