265de1712444aa145ff45012eb329c1d9cae1b2f
ahmershurahbeeljan <ahmershurahbeeljan@gmail.com>
2025-11-25 13:32:42 +0500

    feat: update PackageDetailEntity and PackageDetailModel to use double types for dimensions; improve JSON parsing and default values

--- lib/features/personal/post/feed/views/widgets/post/widgets/section/home_post_icon_botton_section.dart
+++ lib/features/personal/post/feed/views/widgets/post/widgets/section/home_post_icon_botton_section.dart
@@ -15,20 +15,23 @@ class HomePostIconBottonSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final bool isMine = post.createdBy == LocalAuth.uid;
-    return Row(
-      children: <Widget>[
-        if (!isMine || isPreview) CreatePostInquiryChatButton(post: post),
-        SharePostButton(
-          tappableWidget:
-              const CustomSvgIcon(assetPath: AppStrings.selloutShareIcon),
-          postId: post.postID,
-        ),
-        const Spacer(),
-        if (!isMine || isPreview)
-          SavePostIconButton(
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Row(
+        children: <Widget>[
+          if (!isMine || isPreview) CreatePostInquiryChatButton(post: post),
+          SharePostButton(
+            tappableWidget:
+                const CustomSvgIcon(assetPath: AppStrings.selloutShareIcon),
             postId: post.postID,
-          )
-      ],
+          ),
+          const Spacer(),
+          if (!isMine || isPreview)
+            SavePostIconButton(
+              postId: post.postID,
+            )
+        ],
+      ),
     );
   }
 }
