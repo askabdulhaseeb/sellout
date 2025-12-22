98e17459241584f4e5977dbc3dddd49460ff8db2
ahmershurahbeeljan <ahmershurahbeeljan@gmail.com>
2025-12-22 14:32:08 +0500

    feat: Update photo verification text and improve layout in signup flow

--- lib/features/personal/auth/signup/views/screens/pages/signup_photo_verification_page.dart
+++ lib/features/personal/auth/signup/views/screens/pages/signup_photo_verification_page.dart
@@ -14,97 +14,112 @@ class SignupPhotoVerificationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<SignupProvider>(
-        builder: (BuildContext context, SignupProvider pro, _) {
-      return SingleChildScrollView(
-        child: Column(
-          spacing: 12,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'photo_verification',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-            ).tr(),
-            Text(
-              'photo_verification_subtitle',
-              textAlign: TextAlign.center,
-              style: TextTheme.of(context).bodySmall,
-            ).tr(),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4),
-              child: AspectRatio(
+      builder: (BuildContext context, SignupProvider pro, _) {
+        return SingleChildScrollView(
+          child: Column(
+            spacing: 12,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'photo_verification',
+                style: TextTheme.of(
+                  context,
+                ).titleMedium?.copyWith(fontWeight: FontWeight.bold),
+              ).tr(),
+              Text(
+                'photo_verification_subtitle',
+                textAlign: TextAlign.center,
+                style: TextTheme.of(context).bodySmall,
+              ).tr(),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 4),
+                child: AspectRatio(
                   aspectRatio: 1 / 1,
                   child: pro.attachment == null
                       ? Container(
                           decoration: BoxDecoration(
-                              border: Border.all(
-                                  color: Theme.of(context).primaryColor),
-                              borderRadius: BorderRadius.circular(15)),
+                            border: Border.all(
+                              color: Theme.of(context).primaryColor,
+                            ),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
                           child: Column(
                             spacing: 8,
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: <Widget>[
                               Container(
                                 decoration: BoxDecoration(
-                                    color: Theme.of(context)
-                                        .primaryColor
-                                        .withValues(alpha: 0.3),
-                                    // border: Border.all(color: Theme.of(context).primaryColor),
-                                    borderRadius: BorderRadius.circular(15)),
+                                  color: Theme.of(
+                                    context,
+                                  ).primaryColor.withValues(alpha: 0.3),
+                                  // border: Border.all(color: Theme.of(context).primaryColor),
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
                                 padding: const EdgeInsets.all(16),
                                 child: Icon(
                                   CupertinoIcons.person_badge_plus,
                                   color: Theme.of(context).primaryColor,
                                 ),
                               ),
-                              Text('upload_selfie_verification',
-                                      style: TextTheme.of(context).titleSmall)
-                                  .tr(),
+                              Text(
+                                'upload_selfie_verification',
+                                style: TextTheme.of(context).titleSmall,
+                              ).tr(),
                               Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 8.0),
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 8.0,
+                                ),
                                 child: Text(
                                   textAlign: TextAlign.center,
                                   'upload_selfie_description'.tr(),
-                                  style: TextTheme.of(context)
-                                      .bodySmall
+                                  style: TextTheme.of(context).bodySmall
                                       ?.copyWith(
-                                          color: ColorScheme.of(context)
-                                              .outlineVariant),
+                                        color: ColorScheme.of(
+                                          context,
+                                        ).outlineVariant,
+                                      ),
                                 ),
                               ),
                               SizedBox(
                                 width: 120,
                                 child: CustomElevatedButton(
-                                    title: 'take_photo'.tr(),
-                                    textStyle: TextTheme.of(context)
-                                        .bodySmall
-                                        ?.copyWith(
-                                            color: ColorScheme.of(context)
-                                                .onPrimary),
-                                    isLoading: false,
-                                    onTap: () {
-                                      pro.setImage(context,
-                                          type: AttachmentType.image);
-                                    }),
-                              )
+                                  title: 'take_photo'.tr(),
+                                  textStyle: TextTheme.of(context).bodySmall
+                                      ?.copyWith(
+                                        color: ColorScheme.of(
+                                          context,
+                                        ).onPrimary,
+                                      ),
+                                  isLoading: false,
+                                  onTap: () {
+                                    pro.setImage(
+                                      context,
+                                      type: AttachmentType.image,
+                                    );
+                                  },
+                                ),
+                              ),
                             ],
                           ),
                         )
-                      : Image.file(File(pro.attachment?.file.path ?? ''))),
-            ),
-            Text(
-              'photo_verification_policy',
-              textAlign: TextAlign.center,
-              style: TextTheme.of(context).bodySmall,
-            ).tr(),
-            CustomElevatedButton(
+                      : Image.file(File(pro.attachment?.file.path ?? '')),
+                ),
+              ),
+              Text(
+                'photo_verification_policy',
+                textAlign: TextAlign.center,
+                style: TextTheme.of(context).bodySmall,
+              ).tr(),
+              CustomElevatedButton(
                 title: 'verify_photo'.tr(),
                 isLoading: pro.isLoading,
-                onTap: () => pro.onNext(context)),
-            const SizedBox(height: 16),
-          ],
-        ),
-      );
-    });
+                onTap: () => pro.onNext(context),
+              ),
+              const SizedBox(height: 16),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
